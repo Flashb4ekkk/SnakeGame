61e733dc6baa1a436195cac054807f559191dd2f
Bethakzs <romanwdwd@gmail.com>
2023-09-23 15:38:41 +0300

    second commit

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -9,6 +9,9 @@ public class MainWindow extends JFrame {
         add(new StarterWindow()); //new GameArea()
         setVisible(true);
 
+        System.out.println("it's will be try for next commit");
+        System.out.println("123");
+        System.out.println("123");
     }
 }
 
